7c96e496225491123155f4fe6979671eb3cb39c4
Omkar Chandorkar <gotenksIN@aospa.co>
2022-09-01 17:18:58 +0530

    Introduce PropImitationHooks
    - A minimal implementation to pass CTS and Play Integrity while
      loading correct ARCore profiles if existing
    
    - Loosely inspired by PixelPropUtils from
      https://github.com/PixelExperience/frameworks_base/blob/twelve/core/java/com/android/internal/util/custom/PixelPropsUtils.java
    
    - Thanks Danny for original idea at
      https://github.com/ProtonAOSP/android_frameworks_base/blob/sc-qpr3/core/java/com/android/internal/gmscompat/AttestationHooks.java
    
    - Thanks Dyneteve for fixing Play Integrity API at
      https://github.com/hentaiOS/platform_frameworks_base/commit/c9a086f9b635bf4e3e61fbd501ade464f8f5b713
    
    Change-Id: I6937258c8297aa909fcdb3b40195cbd6dc0f869b
    
    Signed-off-by: Omkar Chandorkar <gotenksIN@aospa.co>
    Reviewed-on: https://review.statixos.com/c/android_frameworks_base/+/8571
    Reviewed-by: Anay Wadhera <anay1018@gmail.com>
    Tested-by: Anay Wadhera <anay1018@gmail.com>
    Signed-off-by: spezi77 <spezi7713@gmx.net>
    Co-Authored-By: Chris Crawford <chriscrawford893@gmail.com>
    Co-Authored-By: jhenrique09 <jhenrique09.mcz@hotmail.com>
    Co-Authored-By: Danny Lin <danny@kdrag0n.dev>
    Co-Authored-By: Adithya R <gh0strider.2k18.reborn@gmail.com>

--- services/core/java/com/android/server/wm/ActivityTaskManagerService.java
+++ services/core/java/com/android/server/wm/ActivityTaskManagerService.java
@@ -271,7 +271,6 @@ import com.android.server.uri.NeededUriGrants;
 import com.android.server.uri.UriGrantsManagerInternal;
 
 import com.android.internal.util.xtended.CutoutFullscreenController;
-import com.android.internal.util.xtended.PixelPropsUtils;
 
 import java.io.BufferedReader;
 import java.io.File;
@@ -1975,9 +1974,7 @@ public class ActivityTaskManagerService extends IActivityTaskManager.Stub {
 
     @Override
     public RootTaskInfo getFocusedRootTaskInfo() throws RemoteException {
-        if (!PixelPropsUtils.shouldBypassTaskPermission(mContext)) {
-            enforceTaskPermission("getFocusedRootTaskInfo()");
-        }
+        enforceTaskPermission("getFocusedRootTaskInfo()");
         final long ident = Binder.clearCallingIdentity();
         try {
             synchronized (mGlobalLock) {
@@ -2998,9 +2995,7 @@ public class ActivityTaskManagerService extends IActivityTaskManager.Stub {
     /** Sets the task stack listener that gets callbacks when a task stack changes. */
     @Override
     public void registerTaskStackListener(ITaskStackListener listener) {
-        if (!PixelPropsUtils.shouldBypassTaskPermission(mContext)) {
-            enforceTaskPermission("registerTaskStackListener()");
-        }
+        enforceTaskPermission("registerTaskStackListener()");
         mTaskChangeNotificationController.registerTaskStackListener(listener);
     }
 
